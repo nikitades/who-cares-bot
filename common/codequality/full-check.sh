5d5aff8d3cf188e5f1b6dfe7a807779bdb530172
Nikita Pavlovskiy <nikita.pavlovskiy@carvago.com>
2021-05-14 15:56:46 +0300

    Install phpunit

--- common/codequality/full-check.sh
+++ common/codequality/full-check.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 vendor/bin/php-cs-fixer fix --config ../common/codequality/phpcsfixer.dist.php ./src ./tests
-vendor/bin/phpstan analyse src tests --memory-limit 256M -c ../common/codequality/phpstan.neon
+vendor/bin/phpstan analyse src tests --memory-limit 256M
+vendor/bin/phpunit tests
