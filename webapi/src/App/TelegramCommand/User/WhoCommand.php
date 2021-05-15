86ae01134727258248da8fe1806cb4f5eafc6ecd
Nikita Pavlovskiy <nikita.pavlovskiy@carvago.com>
2021-05-16 00:12:22 +0300

    Basic Who Command implementation

--- webapi/src/App/TelegramCommand/User/WhoCommand.php
+++ webapi/src/App/TelegramCommand/User/WhoCommand.php
@@ -6,17 +6,48 @@ namespace Nikitades\WhoCaresBot\WebApi\App\TelegramCommand\User;
 
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Entities\Update;
 use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Telegram;
+use Nikitades\WhoCaresBot\WebApi\Domain\Query\UserPosition;
+use Nikitades\WhoCaresBot\WebApi\Domain\Query\WhoDay\WhoDayQuery;
+use Nikitades\WhoCaresBot\WebApi\Domain\Query\WhoDay\WhoDayQueryResponse;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class WhoCommand extends UserCommand
 {
+    use HandleTrait;
+
+    public function __construct(
+        Telegram $telegram,
+        Update $update,
+        MessageBusInterface $messageBus
+    ) {
+        parent::__construct($telegram, $update);
+        $this->messageBus = $messageBus;
+    }
+
     public function execute(): ServerResponse
     {
         $message = $this->getMessage();
 
+        /** @var WhoDayQueryResponse $response */
+        $response = $this->handle(new WhoDayQuery(
+                $message->getChat()->getId(),
+                $message->getFrom()->getId()
+            )
+        );
+
         return Request::sendMessage([
             'chat_id' => $message->getChat()->getId(),
-            'text' => 'who!',
+            'text' => implode(
+                "\n",
+                array_map(
+                    fn (UserPosition $userPosition): string => sprintf('%s: %s', $userPosition->userNickname, $userPosition->userMessagesCount),
+                    $response->userPositions
+                ),
+            ),
         ]);
     }
 }
--- webapi/src/Domain/Query/WhoDay/WhoDayQueryHandler.php
+++ webapi/src/Domain/Query/WhoDay/WhoDayQueryHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Nikitades\WhoCaresBot\WebApi\Domain\Query\WhoDay;
 
+use Nikitades\WhoCaresBot\WebApi\Domain\Query\UserPosition;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class WhoDayQueryHandler implements MessageHandlerInterface
@@ -11,7 +12,13 @@ class WhoDayQueryHandler implements MessageHandlerInterface
     public function __invoke(WhoDayQuery $query): WhoDayQueryResponse
     {
         return new WhoDayQueryResponse(
-            [],
+            [
+                new UserPosition(
+                    'someUser',
+                    213412312,
+                    33
+                ),
+            ],
             'a'
         );
     }
