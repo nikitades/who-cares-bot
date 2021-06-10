461402306403b23f9d201d82155774f086948698
Nikita Pavlovskiy <nikita.pavlovskiy@carvago.com>
2021-06-10 12:07:54 +0300

    Update Dockerfiles

--- webapi/docker/entrypoint_migrations.sh
+++ webapi/docker/entrypoint_migrations.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sleep 10;
+sleep 30;
 
 php bin/console doctrine:database:create --if-not-exists --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
